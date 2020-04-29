af63ffe69afa4703a6a19ded0075ec60949eb1a2
Ilya <admin@redguy.ru>
2020-04-30 00:03:28 +0300

    logger

--- core/Core.js
+++ core/Core.js
@@ -3,10 +3,18 @@ class Core {
     }
 
     addTwoWayBridge(chatOne, chatTwo) {
+        this.addTwoWayBridge(chatOne, chatTwo, false);
+    }
+
+    addTwoWayBridge(chatOne, chatTwo, logging) {
         chatOne.setMessageCallBack(function(message, senderInfo) {
-                chatTwo.sendMessage("["+senderInfo.name+"] " + message);
+            if (logging)
+                console.log("["+senderInfo.name+"] " + message);
+            chatTwo.sendMessage("["+senderInfo.name+"] " + message);
         });
         chatTwo.setMessageCallBack(function(message, senderInfo) {
+            if (logging)
+                console.log("["+senderInfo.name+"] " + message);
             chatOne.sendMessage("[" + senderInfo.name + "] " + message);
         });
     }
